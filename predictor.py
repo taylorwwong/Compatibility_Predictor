d148ec5366c39ba94086a49ca4a706fd428cba38
Taylor Wong <taylorsw@hawaii.edu>
2023-03-21 17:23:24 -1000

    Logic

--- predictor.py
+++ predictor.py
@@ -1,5 +1,44 @@
 import json
+import numpy as np
 
 # load file
-file = json.load(open('input.json'))
+data = json.load(open('input.json'))
 
+# setup lists
+team_total = []
+app_total = []
+result = []
+
+# loop through json
+for i in data['team']:
+    intel = (i['attributes']['intelligence'])
+    spicy = (i['attributes']['spicyFoodTolerance'])
+    combined = int(intel) + int(spicy)
+    team_total.append(int(combined))
+
+for i in data['applicants']:
+    intel = (i['attributes']['intelligence'])
+    spicy = (i['attributes']['spicyFoodTolerance'])
+    combined = int(intel) + int(spicy)
+    app_total.append(int(combined))
+
+# find mean for team
+avg_team = np.mean(team_total)
+
+# find compatibility
+for i in app_total:
+    temp = avg_team - i
+    comp = (10 - abs(temp))/10
+    result.append(comp)
+
+
+
+
+# write to json
+with open('output.json', 'w') as out:
+    out.write(obj)
+
+print(team_total)
+print(avg_team)
+print(app_total)
+print(result)
